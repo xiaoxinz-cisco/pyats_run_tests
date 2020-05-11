119b0601fa6d8fccb918eda8599b53074a28cabd
xiaoxinz-cisco <60668974+xiaoxinz-cisco@users.noreply.github.com>
2020-05-11 00:03:43 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -41,6 +41,18 @@ fi
 # -------
 cd ${WORKSPACE}
 
+
+# #!/bin/sh -l
+
+# cd to repo
+cd $GITHUB_WORKSPACE/$1
+
+# install dependencies
+pip3 install -r $2
+
+# test
+python3 -m unittest discover -t $3 -p $4
+
 # handoff
 # -------
 exec "$@"
